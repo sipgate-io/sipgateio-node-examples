a34f6317434dc9f0145ffd10850cad3789074344
Florian Brandt <fbrandt@sipgate.de>
2020-02-13 11:32:47 +0100

    feat: add call example for sipgateio
    Co-Authored-By: Jan Vogt <jvogt@sipgate.de>

---
+++ call/call.ts
@@ -0,0 +1,21 @@
+import { createCallModule, sipgateIO } from 'sipgateio';
+
+const username = process.env.SIPGATE_USERNAME || '';
+const password = process.env.SIPGATE_PASSWORD || '';
+/**
+ * For details on how to instantiate the client, see 'examples/client/client.ts'
+ */
+const client = sipgateIO({ username, password });
+
+const from = process.env.SIPGATE_CALLER || '';
+const to = process.env.SIPGATE_CALLEE || '';
+
+const call = createCallModule(client);
+call
+	.initiate({ to, from })
+	.then(() => {
+		console.log('Call initiated');
+	})
+	.catch(error => {
+		console.error(error.message);
+	});
