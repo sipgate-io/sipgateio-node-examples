6a089ee1f545b47474f1c749a67b6065cc54fa24
Maximilian Massing <massing@sipgate.de>
2020-03-23 16:14:03 +0100

    feat: extend webhook gather example
    Co-authored-by: Florian Brandt <fbrandt@sipgate.de>
    Co-authored-by: Sven Kube <kube@sipgate.de>

--- webhook/webhook_gather.ts
+++ webhook/webhook_gather.ts
@@ -1,25 +1,8 @@
-import {
-	WebhookResponse,
-	createSettingsModule,
-	createWebhookModule,
-	sipgateIO,
-} from 'sipgateio';
+import { WebhookResponse, createWebhookModule } from 'sipgateio';
 
 const port = 8080;
-const serverAddress = process.env.SIPGATE_WEBHOOK_SERVER_ADDRESS || '';
-
-const password = process.env.SIPGATE_PASSWORD || '';
-const username = process.env.SIPGATE_USERNAME || '';
-
-/**
- * For details on how to instantiate the client, see 'examples/client/client.ts'
- */
-const client = sipgateIO({ username, password });
-
-const settingsModule = createSettingsModule(client);
-
-settingsModule.setOutgoingUrl(serverAddress);
-settingsModule.setIncomingUrl(serverAddress);
+const serverAddress =
+	process.env.SIPGATE_WEBHOOK_SERVER_ADDRESS || 'https://example.com:8080';
 
 const webhookModule = createWebhookModule();
 webhookModule
@@ -28,8 +11,18 @@ webhookModule
 		serverAddress,
 	})
 	.then(webhookServer => {
-		console.log(`Server running at ${serverAddress}\nReady for calls 📞`);
+		console.log(
+			`Server running at ${serverAddress}\n` +
+				'Please set this URL for incoming calls at https://console.sipgate.com/webhooks/urls\n' +
+				"ProTip: To see how to do that automatically, check out the example at 'examples/settings/settings_set_url_incoming.ts'\n" +
+				'Ready for calls 📞'
+		);
+
 		webhookServer.onNewCall(newCallEvent => {
+			if (newCallEvent['user[]'].includes('voicemail')) {
+				return;
+			}
+
 			console.log(`New call from ${newCallEvent.from} to ${newCallEvent.to}`);
 
 			return WebhookResponse.gatherDTMF({
@@ -38,9 +31,43 @@ webhookModule
 				announcement: 'https://static.sipgate.com/examples/wav/example.wav',
 			});
 		});
+
 		webhookServer.onData(dataEvent => {
-			console.log(
-				`The caller pressed ${dataEvent.dtmf ? dataEvent.dtmf : 'nothing'}`
-			);
+			const selection = dataEvent.dtmf;
+
+			console.log(`The caller pressed ${selection ? selection : 'nothing'}`);
+
+			const redirectNumbers = ['12'];
+
+			switch (selection) {
+				case '0':
+					console.log('Restarting the loop');
+					return WebhookResponse.gatherDTMF({
+						maxDigits: 1,
+						timeout: 5000,
+						announcement: 'https://static.sipgate.com/examples/wav/example.wav',
+					});
+				case '1':
+					console.log('Redirecting the call to voicemail');
+					return WebhookResponse.sendToVoicemail();
+				case '2':
+					console.log(`Redirecting the call to ${redirectNumbers.join(', ')}`);
+					return WebhookResponse.redirectCall({
+						numbers: redirectNumbers,
+						anonymous: true,
+						callerId: '+49123456789',
+					});
+				case '3':
+					console.log('Hanging up the call');
+					return WebhookResponse.hangUpCall();
+			}
+		});
+
+		webhookServer.onAnswer(answerEvent => {
+			console.log(`The call was answered by ${answerEvent.user}`);
+		});
+
+		webhookServer.onHangUp(hangUpEvent => {
+			console.log(`The call has been hung up with cause ${hangUpEvent.cause}`);
 		});
 	});
