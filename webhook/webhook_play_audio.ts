85a0e2e791f3259dd6590530684e476379087718
Maximilian Massing <massing@sipgate.de>
2020-03-23 16:32:30 +0100

    feat: add webhook example for playing audio
    Co-authored-by: Florian Brandt <fbrandt@sipgate.de>
    Co-authored-by: Sven Kube <kube@sipgate.de>

---
+++ webhook/webhook_play_audio.ts
@@ -0,0 +1,29 @@
+import { WebhookResponse, createWebhookModule } from 'sipgateio';
+
+const port = 8080;
+const serverAddress =
+	process.env.SIPGATE_WEBHOOK_SERVER_ADDRESS || 'https://example.com:8080';
+
+const webhookModule = createWebhookModule();
+webhookModule
+	.createServer({
+		port,
+		serverAddress,
+	})
+	.then(webhookServer => {
+		console.log(
+			`Server running at ${serverAddress}\n` +
+				'Please set this URL for incoming calls at https://console.sipgate.com/webhooks/urls\n' +
+				"ProTip: To see how to do that automatically, check out the example at 'examples/settings/settings_set_url_incoming.ts'\n" +
+				'Ready for calls 📞'
+		);
+
+		webhookServer.onNewCall(newCallEvent => {
+			console.log(`New call from ${newCallEvent.from} to ${newCallEvent.to}`);
+
+			console.log('Playing audio...');
+			return WebhookResponse.playAudio({
+				announcement: 'https://static.sipgate.com/examples/wav/example.wav',
+			});
+		});
+	});
