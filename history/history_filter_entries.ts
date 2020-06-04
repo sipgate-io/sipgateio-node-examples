af081da321ed8bc30a8085409970a0d017e5a4ca
Björn Vopel <vopel@sipgate.de>
2020-06-04 09:01:38 +0200

    feat: add example for filtering history entries
    Co-Authored-By: Max Massing <massing@sipgate.de>
    Co-Authored-By: David Borzek <borzek@sipgate.de>
    Co-Authored-By: Robert Deppe <deppe@sipgate.de>

---
+++ history/history_filter_entries.ts
@@ -0,0 +1,19 @@
+import * as dotenv from 'dotenv';
+import { HistoryEntryType } from 'sipgateio/dist/history';
+import { createHistoryModule, sipgateIO } from 'sipgateio';
+dotenv.config();
+
+const username = process.env.SIPGATE_USERNAME || '';
+const password = process.env.SIPGATE_PASSWORD || '';
+/**
+ * For details on how to instantiate the client, see 'examples/client/client.ts'
+ */
+const client = sipgateIO({ username, password });
+const historyModule = createHistoryModule(client);
+
+historyModule
+	.fetchAll({
+		types: [HistoryEntryType.SMS],
+	})
+	.then(console.log)
+	.catch(console.error);
