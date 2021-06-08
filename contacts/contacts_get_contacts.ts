868187b7a7f0dc416ffcd5280f18bf5b5802de36
Julian Bokelmann <bokelmann@sipgate.de>
2021-06-08 10:55:23 +0200

    add: get contacts example
    Co-authored-by: Manuel Arnold <arnold@sipgate.de>

---
+++ contacts/contacts_get_contacts.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv';
+import { createContactsModule, sipgateIO } from 'sipgateio';
+dotenv.config();
+
+const personalAccessTokenId = process.env.SIPGATE_TOKEN_ID || '';
+const personalAccessToken = process.env.SIPGATE_TOKEN || '';
+
+/**
+ * For details on how to instantiate the client, see 'examples/client/client.ts'
+ */
+const client = sipgateIO({
+	tokenId: personalAccessTokenId,
+	token: personalAccessToken,
+});
+const contacts = createContactsModule(client);
+
+contacts
+	.get('PRIVATE') //PRIVATE, SHARED, INTERNAL
+	.then((response) => {
+		console.log(response);
+	})
+	.catch(console.error);
